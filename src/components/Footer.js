1674a8ac5f0211bc57751001935bd9772142a60f
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2023-04-24 08:23:00 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,22 @@ export default function Footer() {
 
   return (
     <div className="bg-[#516953] w-full py-1">
-      <div className="flex-wrap text-zinc-200 mx-auto flex max-w-7xl  justify-between p-6 lg:px-8">
-        <div className="flex flex-col mx-auto mb-4 w-32 items-start">
+      <div className="flex-wrap text-zinc-200 mx-auto flex max-w-7xl justify-between p-6 lg:px-8">
+        <div className="flex flex-col mb-4 w-32 items-start">
           <h3 className="text-2xl mb-4">Sobre</h3>
           <button onClick={() => router.push("/")}>Nós</button>
           <button onClick={() => router.push("/")}>Senac</button>
         </div>
-        <div className="flex flex-col mx-auto mb-4 w-32 items-start">
+        <div className="flex flex-col mb-4 w-32 items-start">
           <h3 className="text-2xl mb-4">Políticas</h3>
           <button onClick={() => router.push("/")}>Privacidade</button>
           <button onClick={() => router.push("/")}>Responsabilidade</button>
         </div>
-        <div className="flex flex-col mx-auto mb-4 w-32 items-start">
+        <div className="flex flex-col mb-4 w-32 items-start">
           <h3 className="text-2xl mb-4">Apoie</h3>
           <button onClick={() => router.push("/")}>Apoiar</button>
         </div>
-        <div className="flex flex-col mx-auto mb-4 w-32 items-start">
+        <div className="flex flex-col mb-4 w-32 items-start">
           <h3 className="text-2xl mb-4">Nossas Redes</h3>
           <button onClick={() => router.push("/")}>Face</button>
           <button onClick={() => router.push("/")}>Insta</button>

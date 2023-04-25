964da1371fc5d21fe07ab833048a493ea7b2c236
Talles Silva <talles_ramos2012@hotmail.com>
2023-04-25 08:45:46 -0300

    tela de login finalizada - estilização

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,7 @@ function ButtonGoogle() {
   return (
     <Link
       href="/home"
-      className="w-full bg-white active:bg-blueGray-50 mt-10 text-cyan-600 uppercase text-center py-4 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg  hover:border-slate-400 font-bold hover:shadow transition duration-150"
+      className="w-full bg-white active:bg-blueGray-50 mt-10 text-cyan-600 uppercase text-center py-4 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg hover:border-slate-400 font-semibold lg:font-bold hover:shadow transition duration-150"
     >
       <Image
         src={"https://www.svgrepo.com/show/355037/google.svg"}
@@ -22,13 +22,13 @@ function ButtonFacebook() {
   return (
     <Link
       href="/home"
-      className="w-full bg-white active:bg-blueGray-50 mt-5 text-cyan-600 uppercase text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg  hover:border-slate-400 font-bold hover:shadow transition duration-150"
+      className="w-full bg-white active:bg-blueGray-50 mt-5 text-cyan-600 uppercase text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg hover:border-slate-400 font-semibold lg:font-bold hover:shadow transition duration-150"
     >
       <Image
         src={"https://www.svgrepo.com/show/452196/facebook-1.svg"}
         width={1}
         height={1}
-        className="w-8 h-8 mr-5"
+        className="w-8 h-8 mr-3 lg:mr-5"
         alt="Facebook"
       />
       Entrar com Facebook
@@ -38,7 +38,7 @@ function ButtonFacebook() {
 
 export default function LoginForm() {
   return (
-    <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+    <div className="h-2/5 lg:h-full flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
       <div className="flex-1">
         <div className="text-center">
           <h2 className="text-4xl font-bold text-center text-white dark:text-white">
--- src/pages/login.js
+++ src/pages/login.js
@@ -3,18 +3,18 @@ import LoginForm from "@/components/LoginForm";
 export default function Login() {
   return (
     <div className="bg-[#516953]">
-      <div className="flex justify-center h-screen">
+      <div className="lg:flex justify-center h-screen">
         <div
-          className="hidden bg-cover lg:block lg:w-2/3"
+          className="h-3/5 bg-cover lg:block lg:w-2/3 lg:h-screen"
           style={{
             backgroundImage: `url(https://i.pinimg.com/originals/4b/6a/7d/4b6a7d37654c47783ac18eebb9a3218e.png)`,
           }}
         >
-          <div className="flex justify-center items-center text-center h-full px-20 bg-gray-900 bg-opacity-30 ">
-            <div className="bg-neutral-900 bg-opacity-60 p-10 rounded-lg">
+          <div className="flex justify-center items-center text-center h-full px-5 lg:px-20 bg-gray-900 bg-opacity-30 ">
+            <div className="bg-neutral-900 bg-opacity-60 lg:p-10 rounded-lg">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                className="mx-auto"
+                className="my-5 px-5 mx-auto lg:my-0"
                 src="https://i.pinimg.com/originals/da/ec/5d/daec5d092f7d91a04f272d84a8e13921.png"
                 alt="Imagem externa"
               />
@@ -22,7 +22,7 @@ export default function Login() {
                 A rede social favorita para
                 <br /> a adoção de pets.
               </h1>
-              <h1 className="text-2xl uppercase text-white font-normal mt-5">
+              <h1 className="text-2xl uppercase text-white font-normal my-5 lg:my-0 lg:mt-5">
                 Não compre, adote!
               </h1>
             </div>

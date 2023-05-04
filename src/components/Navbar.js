a91a001d33d2fd6ba561f939983179c271cd272c
Talles Silva <talles_ramos2012@hotmail.com>
2023-05-04 09:26:39 -0300

    adicionado botão de sair, reclamar com talles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -87,6 +87,12 @@ export default function Header() {
               <Modal {...apoiaSeModal} />
             )}
           </button>
+          <button
+            onClick={() => router.push("/login")}
+            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-[#5A3938]"
+          >
+            SAIR
+          </button>
         </Popover.Group>
       </nav>
       <Dialog
